a8be4b1bd3103145fe494c24eedf5dffccf82b36
HasanAlii73 <ha1338967@gmail.com>
2025-11-15 14:19:29 +0300

    create ship class

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,28 @@
+const { sharing } = require("webpack");
+
 function sum(a, b) {
   return a + b;
 }
 
-module.exports = sum;
+
+class Ship {
+  constructor(name, length, hitPoints, sunk = false) {
+    this.name = name;
+    this.length = length;
+    this.hitPoints = hitPoints;
+    this.sunk = sunk;
+  }
+
+  hit() {
+    this.hitPoints++;
+    if (this.hitPoints >= this.length) {
+      this.sunk = true;
+    }
+  }
+
+  isSunk() {
+    return this.sunk;
+  }
+}
+
+module.exports = { sum , Ship};
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,5 +1,22 @@
-const sum = require('./ship');
+const {sum, Ship} = require('./ship');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('Ship hit method increments hitPoints and updates sunk status', () => {
+  const ship = new Ship('Destroyer', 3, 0);
+  expect(ship.sunk).toBe(false);
+  
+  ship.hit();
+  expect(ship.hitPoints).toBe(1);
+  expect(ship.isSunk()).toBe(false);
+  
+  ship.hit();
+  expect(ship.hitPoints).toBe(2);
+  expect(ship.isSunk()).toBe(false);
+  
+  ship.hit();
+  expect(ship.hitPoints).toBe(3);
+  expect(ship.isSunk()).toBe(true);
 });
